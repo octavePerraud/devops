d41670830e9d0bed05cd67b9e9317d722a1782e9
octavePerraud <62344928+octavePerraud@users.noreply.github.com>
2020-11-03 17:33:29 +0100

    2script

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,1 @@
-cd ./simple-api
 mvn clean verify org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=octavePerraud_devops
--- script/docker.sh
+++ script/docker.sh
@@ -1,3 +1,11 @@
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t $DOCKER_USERNAME/tp-devops .
-docker push $DOCKER_USERNAME/tp-devops
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+#build
+docker build -t $DOCKER_USERNAME/tp-devops:tp2-simple-api simple-api
+docker build -t $DOCKER_USERNAME/tp-devops:tp2-httpd httpd
+docker build -t $DOCKER_USERNAME/tp-devops:tp2-database database
+
+#push
+docker push $DOCKER_USERNAME/tp-devops:tp2-simple-api
+docker push $DOCKER_USERNAME/tp-devops:tp2-httpd
+docker push $DOCKER_USERNAME/tp-devops:tp2-database
